48579403a9c0e105f8ec17fd473bfd1f9cd2b91e
keithliam <keithliamm@gmail.com>
2018-06-26 15:48:07 +0800

    Add prompt for specific category

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import csv
 import MySQLdb as db
 
-with open(input('Enter filepath: '), 'r', encoding='Latin-1') as f:
+with open('books.csv', 'r', encoding='Latin-1') as f:
 	dataset = list(csv.reader(f))
 
 db = db.connect(host='localhost', user='root', passwd='Amazing!', db='tay_bot')
 
-for i in range(0, int(input('How many books? '))):
-	cur = db.cursor()
-	try:
-		cur.execute("""INSERT INTO book(title, author, category) VALUES (%s,%s,%s)""", (dataset[i][3], dataset[i][4], dataset[i][6]))
-		db.commit()
-	except:     
-		db.rollback()
+num = int(input('How many books? '))
+i = 0
+ii = 0
+
+while i < num and ii < len(dataset):
+	if dataset[ii][6] == 'Teen & Young Adult':
+		cur = db.cursor()
+		try:
+			cur.execute("""INSERT INTO book(title, author, category) VALUES (%s,%s,%s)""", (dataset[ii][3], dataset[ii][4], dataset[ii][6]))
+			db.commit()
+		except:     
+			db.rollback()
+		i += 1
+	ii += 1
 
 print("Done!")
